9760a6960cff1a78ca88707917274b6e0e764730
Tanreet Dhaliwal <tdhali@uwo.ca>
2020-08-23 18:14:00 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3210d7ece53945f1f6f958209736f87c.js"
+  "/portfolio/precache-manifest.1cd5e0a525213791097f6056519e2038.js"
 );
 
 self.addEventListener('message', (event) => {
