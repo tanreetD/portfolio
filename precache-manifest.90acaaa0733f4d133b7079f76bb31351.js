474b8ab7de5ad6229a768f45e9bf1ef1e38a4df5
Tanreet Dhaliwal <tdhali@uwo.ca>
2020-08-23 10:48:21 -0400

    Updates

---
+++ precache-manifest.90acaaa0733f4d133b7079f76bb31351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e24609c276e0edfcdf0ca74a69b6c2b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2f5cf3392054247fe1ee",
+    "url": "/portfolio/static/css/main.8561d4c9.chunk.css"
+  },
+  {
+    "revision": "01d602191e6311dab9e1",
+    "url": "/portfolio/static/js/2.03005c32.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.03005c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f5cf3392054247fe1ee",
+    "url": "/portfolio/static/js/main.1ac3086c.chunk.js"
+  },
+  {
+    "revision": "74184777695d4976f548",
+    "url": "/portfolio/static/js/runtime-main.02293949.js"
+  }
+]);
